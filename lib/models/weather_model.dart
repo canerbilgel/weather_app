0f353e5d32dec12b73ec6d9026d1bb0058814d0c
Caner Bilgel <bilgelcaner@gmail.com>
2025-07-29 11:40:52 +0300

    Add feels-like temperature and 4-day weather forecast display

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,20 +1,23 @@
 class Weather {
-  final String cityName;
+  final String? cityName;
   final double temperature;
   final String description;
   final String iconCode;
   final double windSpeed;
   final int humidity;
-
-
+  final DateTime? date;
+  final double feelsLike;
 
   Weather({
-    required this.cityName,
+    this.cityName,
     required this.temperature,
     required this.description,
     required this.iconCode,
     required this.humidity,
     required this.windSpeed,
+    required this.feelsLike,
+
+    this.date,
   });
 
   factory Weather.fromJson(Map<String, dynamic> json) {
@@ -25,6 +28,20 @@ class Weather {
       iconCode: json['weather'][0]['icon'],
       humidity: json['main']['humidity'],
       windSpeed: json['wind']['speed'].toDouble(),
+      feelsLike: json['main']['feels_like'].toDouble(),
+
+    );
+  }
+
+  factory Weather.fromForecastJson(Map<String, dynamic> json) {
+    return Weather(
+      temperature: json['main']['temp'].toDouble(),
+      description: json['weather'][0]['description'],
+      iconCode: json['weather'][0]['icon'],
+      humidity: json['main']['humidity'],
+      windSpeed: json['wind']['speed'].toDouble(),
+      date: DateTime.parse(json['dt_txt']),
+      feelsLike: json['main']['feels_like'].toDouble(),
 
     );
   }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -7,6 +7,7 @@ class WeatherService {
   final String? _apiKey = dotenv.env['OPENWEATHER_API_KEY'];
   final String? _baseUrl = dotenv.env['OPENWEATHER_BASE_URL'];
 
+  // Anlık hava durumu
   Future<Weather> fetchWeather(String cityName) async {
     final url = Uri.parse(
       '$_baseUrl/weather?q=$cityName&appid=$_apiKey&units=metric&lang=en',
@@ -18,13 +19,32 @@ class WeatherService {
       final data = jsonDecode(response.body);
       return Weather.fromJson(data);
     } else if (response.statusCode == 401) {
-      throw Exception('API anahtarı geçersiz veya yetki hatası.');
+      throw Exception('API anahtarı geçersiz.');
     } else if (response.statusCode == 404) {
       throw Exception('Şehir bulunamadı: $cityName');
     } else {
-      throw Exception(
-        'Hava durumu alınamadı (HTTP ${response.statusCode}): ${response.reasonPhrase}',
-      );
+      throw Exception('Hata kodu: ${response.statusCode}');
+    }
+  }
+
+  // 4 günlük tahmini veri
+  Future<List<Weather>> fetchForecastByCity(String cityName) async {
+    final url = Uri.parse(
+      '$_baseUrl/forecast?q=$cityName&appid=$_apiKey&units=metric&lang=en',
+    );
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final List list = data['list'];
+
+      // Günde 8 veri geliyor, biz her 24 saatte bir olanları alıyoruz
+      final filtered = [list[0], list[8], list[16], list[24]];
+
+      return filtered.map((item) => Weather.fromForecastJson(item)).toList();
+    } else {
+      throw Exception('Forecast alınamadı: ${response.statusCode}');
     }
   }
 }
